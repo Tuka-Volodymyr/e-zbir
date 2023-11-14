8523a8d694ca12d101d9c1489d3d2f5551e1e428
Demyan <demuanivanytsky8@gmail.com>
2023-11-14 16:37:46 +0200

    Test

---
+++ client/src/components/app/App.js
@@ -0,0 +1,15 @@
+import HeaderLoginForm from "../login-form/HeaderLoginForm";
+import FooterLoginForm from "../login-form/FooterLoginForm";
+
+import '../../index.css';
+
+function App() {
+    return (
+        <div className="wrapper">
+            <HeaderLoginForm />
+            <FooterLoginForm/>
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/components/login-form/HeaderLoginForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HeaderLoginForm() {
+  return (
+      <header>
+            <div>
+                <span className="logo">Logo</span>
+            </div>
+      </header>
+  )
+}
