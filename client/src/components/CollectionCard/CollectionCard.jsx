1e38fadfaa34674aa33a99e3a4cd956a650a67e1
ruslan_kuchmynda <ruslankuchmynda@gmail.com>
2023-11-16 01:35:59 +0200

    Add MailPage Add Social Network Icons Add CollectionCard Change UI «Header.js» Add folder «img»

---
+++ client/src/components/CollectionCard/CollectionCard.jsx
@@ -0,0 +1,12 @@
+import CollectionCardCss from './CollectionCard.module.css'
+const CollectionCard = (props) =>{
+    return(
+    <div className = {CollectionCardCss.content}>
+        <h2 className={CollectionCardCss.nameZbir}>{props.nameZbir}</h2>
+        <h3>{props.name}</h3>
+
+    </div>
+    );
+};
+
+export default CollectionCard;
