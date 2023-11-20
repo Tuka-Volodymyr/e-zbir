1f3ca7afcf886b7b010f1d137456b37be9b797a1
TukaVolodymyr <tukavolody@gmail.com>
2023-11-20 21:34:16 +0200

    add some categories to enum

--- src/main/java/com/ua/ezbir/web/fundraiser/Category.java
+++ src/main/java/com/ua/ezbir/web/fundraiser/Category.java
@@ -1,7 +1,18 @@
 package com.ua.ezbir.web.fundraiser;
 
 public enum Category {
-    ARMY,
-    MIGRANT,
+    Medical_Supplies_Equipment,
+    Support_Military_Forces,
+    Psychological_Support,
+    Education_Training,
+    Human_Rights_Protection,
+    Emergency_Medical_Assistance,
+    Child_Protection,
+    Environmental_Safety,
+    Cyber_Security,
+    Infrastructure_Restoration,
+    Housing_Conditions,
+    Support_Vulnerable_Groups,
+    Information_Security,
     OTHER
 }
