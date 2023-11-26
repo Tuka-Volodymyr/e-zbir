a173ffe483f566be2c080a34ff723678625a8517
Ruslan <gren.ruslan@gmail.com>
2023-11-26 15:12:48 +0200

    add fuzzy search

--- src/main/java/com/ua/ezbir/domain/User.java
+++ src/main/java/com/ua/ezbir/domain/User.java
@@ -1,7 +1,6 @@
 package com.ua.ezbir.domain;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- src/main/java/com/ua/ezbir/repository/UserRepository.java
+++ src/main/java/com/ua/ezbir/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.ua.ezbir.repository;
 
 import com.ua.ezbir.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,8 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    // range fuzzy search 0.0 - 1.0 (1 - complete identity and 0 - no similarity)
+    @Query(value = "SELECT * FROM users WHERE similarity(username, ?1) > 0.3", nativeQuery = true)
     List<User> findByUsernameContainingIgnoreCase(String username);
 }
