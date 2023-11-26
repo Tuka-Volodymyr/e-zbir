9943eabc9d4d36c8e982f783f76b161caab0f17e
ruslan_kuchmynda <ruslankuchmynda@gmail.com>
2023-11-26 16:07:24 +0200

    Add

--- client/src/components/ CheckForm/CheckForm.js
+++ client/src/components/ CheckForm/CheckForm.js
@@ -3,8 +3,12 @@ import {useDispatch, useSelector} from "react-redux";
 import CheckFormCss from './CheckForm.module.css'
 import {useForm} from "react-hook-form";
 import axios from "axios";
+import React, {useState} from "react";
+import {Navigate} from "react-router-dom";
 
 const CheckForm = (props) =>{
+
+    const [status, setStatus] = useState(0)
 
 
     const {
@@ -23,9 +27,9 @@ const CheckForm = (props) =>{
 
         axios.post('http://localhost:8080/check/code?code=' + `${data.code}`,{
             code: data.code,
-        })
+        },{withCredentials: true /* Дозволяє передачу сесійних куки */})
             .then((response) => {
-                console.log(response)
+                setStatus(response.status)
             })
             .catch((error) => {
                 console.log(error)
@@ -39,6 +43,8 @@ const CheckForm = (props) =>{
 
     return(
     <div className = {CheckFormCss.content}>
+        <div>{status === 200 ? <Navigate to='/login'/> : ""}</div>{/*Redirect to checkForm*/}
+
         <div className={CheckFormCss.text}>
             <p>Для підтвердження реєстрації введіть код який був надісланий на вашу пошту "{email}"</p>
         </div>
