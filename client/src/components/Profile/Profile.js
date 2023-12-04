640b986f0e130925cc009d637717a35b8aa09fd6
ruslan_kuchmynda <ruslankuchmynda@gmail.com>
2023-12-04 16:40:52 +0200

    Refactoring

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -10,22 +10,6 @@ import {Navigate} from "react-router-dom";
 import {LoadingOutlined, PlusOutlined} from "@ant-design/icons";
 import axios from "axios";
 
-const getBase64 = (img, callback) => {
-    const reader = new FileReader();
-    reader.addEventListener('load', () => callback(reader.result));
-    reader.readAsDataURL(img);
-};
-const beforeUpload = (file) => {
-    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-    if (!isJpgOrPng) {
-        message.error('You can only upload JPG/PNG file!');
-    }
-    const isLt2M = file.size / 1024 / 1024 < 2;
-    if (!isLt2M) {
-        message.error('Image must smaller than 2MB!');
-    }
-    return isJpgOrPng && isLt2M;
-};
 const Profile = (props) =>{
     const [selectedFile, setSelectedFile] = useState(null);
 
@@ -73,36 +57,8 @@ const Profile = (props) =>{
 
             <div className={ProfileCss.profile}>
                 <div className={ProfileCss.userAvatar}>
-                    {/*<Avatar shape="square" size={256} icon='G'></Avatar>*/}
                     <div className={ProfileCss.avatar}>
-                        {/*<ImgCrop rotationSlider>*/}
-                        {/*    <Upload*/}
-                        {/*        name="avatar"*/}
-                        {/*        listType="picture"*/}
-                        {/*        className={ProfileCss.avatarUploader}*/}
-                        {/*        showUploadList={false}*/}
-                        {/*        action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"*/}
-                        {/*        beforeUpload={beforeUpload}*/}
-                        {/*        onChange={handleChange}*/}
-                        {/*    >*/}
-                        {/*        <div className={ProfileCss.block}>*/}
-                        {/*            {imageUrls ? (*/}
-                        {/*                <img*/}
-                        {/*                    src={imageUrls}*/}
-                        {/*                    alt="avatar"*/}
-                        {/*                    style={{*/}
-                        {/*                        width: '256px',*/}
-                        {/*                        height: '256px',*/}
-                        {/*                        borderRadius: "10px",*/}
-                        {/*                        border: "2px solid #333",*/}
-                        {/*                    }}*/}
-                        {/*                />*/}
-                        {/*            ) : (*/}
-                        {/*                uploadButton*/}
-                        {/*            )}*/}
-                        {/*        </div>*/}
-                        {/*    </Upload>*/}
-                        {/*</ImgCrop>*/}
+
                         <div>
                             <input type="file" onChange={handleFileChange} accept="image/*" />
                             <button onClick={handleUpload}>Відправити фото</button>
